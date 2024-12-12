f95dc181d19044bb5ca2b92b0b6a0d48753c05f1
nima14 <nimataghidoost@gmail.com>
2024-12-12 17:31:16 +0100

    installed package

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
-* Exporra - Freelancer v7.0.7 (https://startbootstrap.com/theme/freelancer)
-* Copyright 2013-2023 Exporra
+* Start Bootstrap - Freelancer v7.0.7 (https://startbootstrap.com/theme/freelancer)
+* Copyright 2013-2024 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
 */
 //

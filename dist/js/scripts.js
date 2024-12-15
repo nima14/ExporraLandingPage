0e6841be3cddfa842f8c690e5776459f898facb6
Nima Taghidoust <NimaTaghidoost@gmail.com>
2024-12-15 13:10:20 +0100

    text related to exporra are added to pug

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Freelancer v7.0.7 (https://startbootstrap.com/theme/freelancer)
+* Start Bootstrap - Exporra v7.0.7 (https://startbootstrap.com/theme/freelancer)
 * Copyright 2013-2024 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/Exporra/blob/master/LICENSE)
 */
 //
 // Scripts
